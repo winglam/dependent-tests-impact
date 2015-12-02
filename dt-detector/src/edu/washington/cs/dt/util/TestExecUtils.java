cb1c34210054335508fabcb5cdc6064113522ee1
winglam <wing.c.lam@gmail.com>
2015-12-02 12:53:19 -0600

    Script for prioritization now complete.

--- dt-detector/src/edu/washington/cs/dt/util/TestExecUtils.java
+++ dt-detector/src/edu/washington/cs/dt/util/TestExecUtils.java
@@ -44,9 +44,9 @@ public class TestExecUtils {
         List<String> commandList = new LinkedList<String>();
         commandList.add("java");
         commandList.add("-cp");
-//        commandList.add(classPath + Globals.pathSep + System.getProperties().getProperty("java.class.path", null));
+        commandList.add(classPath + Globals.pathSep + System.getProperties().getProperty("java.class.path", null));
 //        commandList.add(classPath + Globals.pathSep + System.getProperties().getProperty("java.class.path", null) + "/Users/winglam/GoogleDrive/research/dt-impact/experiments/experiments/crystalvc/impact-tools/*:/Users/winglam/GoogleDrive/research/dt-impact/experiments/experiments/crystalvc/bin/");
-        commandList.add(classPath + Globals.pathSep + System.getProperties().getProperty("java.class.path", null) + "/mnt/hgfs/winglam/GoogleDrive/research/dt-impact/experiments/experiments/crystalvc/impact-tools/*:/mnt/hgfs/winglam/GoogleDrive/research/dt-impact/experiments/experiments/crystalvc/bin/");
+//        commandList.add(classPath + Globals.pathSep + System.getProperties().getProperty("java.class.path", null) + "/mnt/hgfs/winglam/GoogleDrive/research/dt-impact/experiments/experiments/crystalvc/impact-tools/*:/mnt/hgfs/winglam/GoogleDrive/research/dt-impact/experiments/experiments/crystalvc/bin/");
 
         if(tests.size() < threshhold) {
             commandList.add("edu.washington.cs.dt.util.TestRunnerWrapper");
