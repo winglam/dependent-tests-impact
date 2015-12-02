4e3adef0fb58ffe355e1c5f275e14792fe99aa6a
winglam <wing.c.lam@gmail.com>
2015-12-02 12:53:19 -0600

    Script for prioritization now complete.

--- experiments/config.sh
+++ experiments/config.sh
@@ -50,6 +50,19 @@ function runRandom() {
   echo "" >> $1-tp-summary.txt
 }
 
+function runPrioritizationWrapper() {
+  DTChainCoverages=(statement)
+  for i in "${DTChainCoverages[@]}"; do
+    for j in "${orders[@]}"; do
+        if [ "$4" = true ] ; then
+          java -cp $2 edu.washington.cs.dt.impact.Main.Wrapper -technique prioritization -coverage $i -order $j -resolveDependences -origOrder $1-$3-order -testInputDir sootTestOutput -filesToDelete $1-env-files -outputFile $1-$3-wrapper-results
+        else
+          java -cp $2 edu.washington.cs.dt.impact.Main.Wrapper -technique prioritization -coverage $i -order $j -origOrder $1-$3-order -testInputDir sootTestOutput -filesToDelete $1-env-files -outputFile $1-$3-wrapper-results
+        fi
+    done
+  done
+}
+
 function runCoveragesOrders() {
   DTChainCoverages=(statement)
   for i in "${DTChainCoverages[@]}"; do
