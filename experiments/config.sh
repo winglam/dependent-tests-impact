19ea515382843a4044df4cec16ebaa44b210e9c7
Wing Lam <wing.c.lam@gmail.com>
2016-01-20 01:27:33 -0600

    Updating config and selection-runner

--- experiments/config.sh
+++ experiments/config.sh
@@ -7,12 +7,36 @@ machines=(2 4 8 16)
 parallelOrders=($testType-order time)
 selectionOrders=(absolute relative)
 
-directories=(crystalvc dynoptic jfreechart-1.0.15 jodatime-b609d7d66d xml-security-orig-v1)
-experiments=(crystal synoptic jfreechart jodatime xml-security)
-testTypes=(orig auto)
-experimentsCP=(impact-tools/*:bin/:lib/* impact-tools/*:bin/:../synoptic/lib/*:../synoptic/bin/:../daikonizer/bin/ impact-tools/*:bin/:lib/* impact-tools/*:bin/:resources/:lib/* impact-tools/*:bin/:../xml-security-commons/bin/:data/:../xml-security-commons/libs/*)
-sootCP=(impact-tools/*:sootOutput/:lib/* impact-tools/*:sootOutput/:../synoptic/lib/*:../synoptic/bin/:../daikonizer/bin/ impact-tools/*:sootOutput/:lib/* impact-tools/*:sootOutput/:resources/:lib/* impact-tools/*:sootOutput/:../xml-security-commons/bin/:data/:../xml-security-commons/libs/*)
-compileCP=(bin/:lib/* bin/:../synoptic/lib/*:../synoptic/bin/:../daikonizer/bin/ impact-tools/*:bin/:lib/* bin/:resources/:lib/* bin/:../xml-security-commons/bin/:data/:../xml-security-commons/libs/*)
+#experiments=(crystal synoptic jfreechart jodatime xml-security)
+#testTypes=(orig auto)
+#coverages=(statement)
+experiments=(crystal synoptic xml-security)
+testTypes=(auto)
+
+#directories=(crystalvc dynoptic jfreechart-1.0.15 jodatime-b609d7d66d xml-security-orig-v1)
+#experimentsCP=(impact-tools/*:bin/:lib/* impact-tools/*:bin/:../synoptic/lib/*:../synoptic/bin/:../daikonizer/bin/ impact-tools/*:bin/:lib/* impact-tools/*:bin/:resources/:lib/* impact-tools/*:bin/:../xml-security-commons/bin/:data/:../xml-security-commons/libs/*)
+#sootCP=(impact-tools/*:sootOutput/:lib/* impact-tools/*:sootOutput/:../synoptic/lib/*:../synoptic/bin/:../daikonizer/bin/ impact-tools/*:sootOutput/:lib/* impact-tools/*:sootOutput/:resources/:lib/* impact-tools/*:sootOutput/:../xml-security-commons/bin/:data/:../xml-security-commons/libs/*)
+directories=(crystalvc dynoptic xml-security-orig-v1)
+experimentsCP=(impact-tools/*:bin/:lib/* impact-tools/*:bin/:../synoptic/lib/*:../synoptic/bin/:../daikonizer/bin/ impact-tools/*:bin/:../xml-security-commons/bin/:data/:../xml-security-commons/libs/*)
+sootCP=(impact-tools/*:sootOutput/:lib/* impact-tools/*:sootOutput/:../synoptic/lib/*:../synoptic/bin/:../daikonizer/bin/ impact-tools/*:sootOutput/:../xml-security-commons/bin/:data/:../xml-security-commons/libs/*)
+
+
+#newDirectories=(crystal dynoptic-ea407ba0a750 jfreechart-1.0.16 jodatime-d6791cb5f9 xml-security-1_2_0)
+#newExperimentsCP=(crystal/impact-tools/*:crystal/bin/:crystal/libs/lib/* dynoptic-ea407ba0a750/impact-tools/*:dynoptic-ea407ba0a750/bin/:synoptic/lib/*:synoptic-ea407ba0a750/bin/:daikonizer-ea407ba0a750/bin/ jfreechart-1.0.16/impact-tools/*:jfreechart-1.0.16/bin/:jfreechart-1.0.16/lib/* jodatime-d6791cb5f9/impact-tools/*:jodatime-d6791cb5f9/bin/:jodatime-d6791cb5f9/resources/:jodatime-d6791cb5f9/lib/* xml-security-1_2_0/impact-tools/*:xml-security-1_2_0/bin/:xml-security-commons/bin/:xml-security-1_2_0/data/:xml-security-commons/libs/*)
+#instrumentNewExperimentsCP=(impact-tools/*:bin/:libs/lib/* impact-tools/*:bin/:../synoptic/lib/*:../synoptic-ea407ba0a750/bin/:../daikonizer-ea407ba0a750/bin/ impact-tools/*:bin/:lib/* impact-tools/*:bin/:resources/:lib/* impact-tools/*:bin/:../xml-security-commons/bin/:data/:../xml-security-commons/libs/*)
+#oldExperimentsCP=(crystalvc/impact-tools/*:crystalvc/bin/:crystalvc/lib/* dynoptic/impact-tools/*:dynoptic/bin/:synoptic/lib/*:synoptic/bin/:daikonizer/bin/ jfreechart-1.0.15/impact-tools/*:jfreechart-1.0.15/bin/:jfreechart-1.0.15/lib/* jodatime-b609d7d66d/impact-tools/*:jodatime-b609d7d66d/bin/:jodatime-b609d7d66d/resources/:jodatime-b609d7d66d/lib/* xml-security-orig-v1/impact-tools/*:xml-security-orig-v1/bin/:xml-security-commons/bin/:xml-security-orig-v1/data/:xml-security-commons/libs/*)
+
+newDirectories=(crystal dynoptic-ea407ba0a750 xml-security-1_2_0)
+newExperimentsCP=(crystal/impact-tools/*:crystal/bin/:crystal/libs/lib/* dynoptic-ea407ba0a750/impact-tools/*:dynoptic-ea407ba0a750/bin/:synoptic/lib/*:synoptic-ea407ba0a750/bin/:daikonizer-ea407ba0a750/bin/ xml-security-1_2_0/impact-tools/*:xml-security-1_2_0/bin/:xml-security-commons/bin/:xml-security-1_2_0/data/:xml-security-commons/libs/*)
+instrumentNewExperimentsCP=(impact-tools/*:bin/:libs/lib/* impact-tools/*:bin/:../synoptic/lib/*:../synoptic-ea407ba0a750/bin/:../daikonizer-ea407ba0a750/bin/ impact-tools/*:bin/:../xml-security-commons/bin/:data/:../xml-security-commons/libs/*)
+oldExperimentsCP=(crystalvc/impact-tools/*:crystalvc/bin/:crystalvc/lib/* dynoptic/impact-tools/*:dynoptic/bin/:synoptic/lib/*:synoptic/bin/:daikonizer/bin/ xml-security-orig-v1/impact-tools/*:xml-security-orig-v1/bin/:xml-security-commons/bin/:xml-security-orig-v1/data/:xml-security-commons/libs/*)
+
+#compileCP=(bin/:lib/* bin/:../synoptic/lib/*:../synoptic/bin/:../daikonizer/bin/ impact-tools/*:bin/:lib/* bin/:resources/:lib/* bin/:../xml-security-commons/bin/:data/:../xml-security-commons/libs/*)
+#newCompileCP=(bin/:lib/* bin/:../synoptic/lib/*:../synoptic/bin/:../daikonizer/bin/ impact-tools/*:bin/:lib/* bin/:resources/:lib/* bin/:../xml-security-commons/bin/:data/:../xml-security-commons/libs/*)
+
+compileCP=(bin/:lib/* bin/:../synoptic/lib/*:../synoptic/bin/:../daikonizer/bin/ bin/:../xml-security-commons/bin/:data/:../xml-security-commons/libs/*)
+newCompileCP=(bin/:lib/* bin/:../synoptic/lib/*:../synoptic/bin/:../daikonizer/bin/ bin/:../xml-security-commons/bin/:data/:../xml-security-commons/libs/*)
+
 
 function clearTemp() {
   rm -rf sootOutput
@@ -31,11 +55,6 @@ function clearSelectionTemp() {
   rm -rf $2/selectionOutput
   rm -rf $2/tmpfile.txt
   rm -rf $2/tmptestfiles.txt
-  cd $1
-  clearEnv
-  cd ../$2
-  clearEnv
-  cd ..
 }
 
 function instrumentFiles() {
@@ -43,28 +62,29 @@ function instrumentFiles() {
   java -cp $1 edu.washington.cs.dt.impact.Main.InstrumentationMain -inputDir bin
 }
 
-function compileSource() {
+function compileNewSource() {
   index=0
   count=${#experiments[@]}
   while [ "$index" -lt "$count" ]; do
-    echo -e "Compiling experiment: ${experiments[$index]}"
-    cd ${directories[$index]}
-    rm -rf bin/
-    mkdir bin/
-    javac -cp ${compileCP[$index]} -d bin src/**/*.java -Xlint:none -encoding iso-8859-1
-    javac -cp ${compileCP[$index]} -d bin tests/**/*.java -Xlint:none -encoding iso-8859-1
+    echo ""
+    echo -e "Compiling new experiment: ${experiments[$index]} in ${newDirectories[$index]}/"
+    cd ${newDirectories[$index]}
+    ant
     let "index++"
     cd ..
   done
 }
 
-function runPrioritizationWrapper() {
-  DTChainCoverages=(statement)
-  for i in "${DTChainCoverages[@]}"; do
-    for j in "${orders[@]}"; do
-        java -cp $2 edu.washington.cs.dt.impact.Main.Wrapper -technique prioritization -coverage $i -order $j -resolveDependences -origOrder $1-$3-order -testInputDir sootTestOutput -filesToDelete $1-env-files -outputFile $1-$3-DT
-        java -cp $2 edu.washington.cs.dt.impact.Main.Wrapper -technique prioritization -coverage $i -order $j -origOrder $1-$3-order -testInputDir sootTestOutput -filesToDelete $1-env-files -outputFile $1-$3
-    done
+function compileSource() {
+  index=0
+  count=${#experiments[@]}
+  while [ "$index" -lt "$count" ]; do
+    echo ""
+    echo -e "Compiling experiment: ${experiments[$index]} in ${directories[$index]}/"
+    cd ${directories[$index]}
+    ant
+    let "index++"
+    cd ..
   done
 }
 
--- experiments/selection-runner.sh
+++ experiments/selection-runner.sh
@@ -11,34 +11,36 @@ while [ "$index" -lt "$count" ]; do
   echo -e "Starting experiment: ${experiments[$index]}"
 
   for k in "${testTypes[@]}"; do
-    # instrument new subject
+    echo 'Instrumenting new subject'
     cd ${newDirectories[$index]}
-    #java -cp ${newExperimentsCP[$index]} edu.washington.cs.dt.impact.Main.InstrumentationMain -inputDir bin -technique selection
-    # instrument old subject
+    java -cp ${instrumentNewExperimentsCP[$index]} edu.washington.cs.dt.impact.Main.InstrumentationMain -inputDir bin -technique selection
+    echo 'Instrumenting old subject'
     cd ../${directories[$index]}
-    #java -cp ${experimentsCP[$index]} edu.washington.cs.dt.impact.Main.InstrumentationMain -inputDir bin -technique selection
+    java -cp ${experimentsCP[$index]} edu.washington.cs.dt.impact.Main.InstrumentationMain -inputDir bin -technique selection
 
-    # generate sootTestOutput on old subject
-    #java -cp ${sootCP[$index]} edu.washington.cs.dt.main.ImpactMain -inputTests ../${directories[$index]}/${experiments[$index]}-$k-order
+    echo 'Generating sootTestOutput on old subject'
+    java -cp ${sootCP[$index]} edu.washington.cs.dt.main.ImpactMain -inputTests ../${directories[$index]}/${experiments[$index]}-$k-order
     cd ..
 
     for i in "${coverages[@]}"; do
-      java -cp ${newExperimentsCP[$index]} edu.washington.cs.dt.impact.Main.Wrapper -technique prioritization -coverage $i -order original -origOrder ${newDirectories[$index]}/${experiments[$index]}-$k-order  -testInputDir ${directories[$index]}/sootTestOutput -filesToDelete ${newDirectories[$index]}/${experiments[$index]}-env-files -outputFile ${experiments[$index]}-$k-selection-$i-original
+      #java -cp ${newExperimentsCP[$index]} edu.washington.cs.dt.impact.Main.Wrapper -technique prioritization -coverage $i -order original -origOrder ${newDirectories[$index]}/${experiments[$index]}-$k-order  -testInputDir ${directories[$index]}/sootTestOutput -filesToDelete ${newDirectories[$index]}/${experiments[$index]}-env-files -outputFile ${experiments[$index]}-$k-selection-$i-original
       for j in "${selectionOrders[@]}"; do
         # run selection on V1
-        #java -cp ${oldExperimentsCP[$index]} edu.washington.cs.dt.impact.Main.Wrapper -technique selection -coverage $i -order $j -resolveDependences -origOrder ${directories[$index]}/${experiments[$index]}-$k-order -testInputDir ${directories[$index]}/sootTestOutput -filesToDelete ${directories[$index]}/${experiments[$index]}-env-files -outputFile ${experiments[$index]}-$k-DT-selection-oldVers-$i-$j -oldVersCFG ${directories[$index]}/selectionOutput -newVersCFG ${newDirectories[$index]}/selectionOutput
+        echo 'Running selection on V1 with resolveDependences'
+        java -cp ${oldExperimentsCP[$index]} edu.washington.cs.dt.impact.Main.Wrapper -technique selection -coverage $i -order $j -resolveDependences -origOrder ${directories[$index]}/${experiments[$index]}-$k-order -testInputDir ${directories[$index]}/sootTestOutput -filesToDelete ${directories[$index]}/${experiments[$index]}-env-files -outputFile ${experiments[$index]}-$k-DT-selection-oldVers-$i-$j -oldVersCFG ${directories[$index]}/selectionOutput -newVersCFG ${newDirectories[$index]}/selectionOutput
+        #echo 'Running selection on V1 without resolveDependences'
+        #java -cp ${oldExperimentsCP[$index]} edu.washington.cs.dt.impact.Main.Wrapper -technique selection -coverage $i -order $j -origOrder ${directories[$index]}/${experiments[$index]}-$k-order -testInputDir ${directories[$index]}/sootTestOutput -filesToDelete ${directories[$index]}/${experiments[$index]}-env-files -outputFile ${experiments[$index]}-$k-selection-oldVers-$i-$j -oldVersCFG ${directories[$index]}/selectionOutput -newVersCFG ${newDirectories[$index]}/selectionOutput
 
         # run selection on V2
-        #java -cp ${newExperimentsCP[$index]} edu.washington.cs.dt.impact.Main.Wrapper -technique selection -coverage $i -order $j -resolveDependences -origOrder ${newDirectories[$index]}/${experiments[$index]}-$k-order -testInputDir ${directories[$index]}/sootTestOutput -filesToDelete ${newDirectories[$index]}/${experiments[$index]}-env-files -outputFile ${experiments[$index]}-$k-DT-selection-newVers-$i-$j -oldVersCFG ${directories[$index]}/selectionOutput -newVersCFG ${newDirectories[$index]}/selectionOutput
+        #java -cp ${newExperimentsCP[$index]} edu.washington.cs.dt.impact.Main.Wrapper -technique selection -coverage $i -order $j -resolveDependences -origOrder ${newDirectories[$index]}/${experiments[$index]}-$k-order -testInputDir ${directories[$index]}/sootTestOutput -filesToDelete ${newDirectories[$index]}/${experiments[$index]}-env-files -outputFile ${experiments[$index]}-$k-DT-selection-newVers-$i-$j -oldVersCFG ${directories[$index]}/selectionOutput -newVersCFG ${newDirectories[$index]}/selectionOutput -dependentTestFile crystal-auto-dt-statement-relative
         #java -cp ${newExperimentsCP[$index]} edu.washington.cs.dt.impact.Main.Wrapper -technique selection -coverage $i -order $j -origOrder ${newDirectories[$index]}/${experiments[$index]}-$k-order -testInputDir ${directories[$index]}/sootTestOutput -filesToDelete ${newDirectories[$index]}/${experiments[$index]}-env-files -outputFile ${experiments[$index]}-$k-selection-newVers-$i-$j -oldVersCFG ${directories[$index]}/selectionOutput -newVersCFG ${newDirectories[$index]}/selectionOutput
 
         ARRAY+=(${directories[$index]}/${experiments[$index]}-$k-selection-$i-$j)
       done
     done
-    #clearTemp
+    clearSelectionTemp ${directories[$index]} ${newDirectories[$index]}
   done
 
-  #clearSelectionTemp ${directories[$index]} ${newDirectories[$index]}
   let "index++"
 done
 
