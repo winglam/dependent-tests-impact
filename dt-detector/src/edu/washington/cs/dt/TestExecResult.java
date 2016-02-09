43003fe30aa78b14a486654ba015882aeed3a51a
Wing Lam <wing.c.lam@gmail.com>
2016-02-08 19:24:28 -0600

    Formatting changes.

--- dt-detector/src/edu/washington/cs/dt/TestExecResult.java
+++ dt-detector/src/edu/washington/cs/dt/TestExecResult.java
@@ -1,6 +1,7 @@
-/** Copyright 2012 University of Washington. All Rights Reserved.
- *  @author Sai Zhang
- */ 
+/**
+ * Copyright 2012 University of Washington. All Rights Reserved.
+ * @author Sai Zhang
+ */
 package edu.washington.cs.dt;
 
 import java.util.HashMap;
@@ -13,65 +14,64 @@ import java.util.Map;
  * to see the exactly error has been thrown
  * */
 public class TestExecResult {
-	public final Map<String, OneTestExecResult> singleRun;
+    public final Map<String, OneTestExecResult> singleRun;
 
-	protected List<String> allTests = new LinkedList<String>();
-	
-	/* The classified result after running all tests */
-	protected List<String> passingTestsInOrder = new LinkedList<String>();
-	protected List<String> failingTestsInOrder = new LinkedList<String>();
-	protected List<String> errorTestsInOrder = new LinkedList<String>();
-	protected Map<String, RESULT> nameToResultsMap = new HashMap<String, RESULT>();
+    protected List<String> allTests = new LinkedList<String>();
 
-	
-	public TestExecResult(Map<String, OneTestExecResult> singleRun) {
-		this.singleRun = singleRun;
-		//classify each test
-		for(String test : this.singleRun.keySet()) {
-			allTests.add(test);
-			//check the results
-			OneTestExecResult r = this.singleRun.get(test);
-			if(r.result.equals(RESULT.PASS)) {
-				this.passingTestsInOrder.add(test);
-				nameToResultsMap.put(test, RESULT.PASS);
-			} else if (r.result.equals(RESULT.FAILURE)) {
-				this.failingTestsInOrder.add(test);
-				nameToResultsMap.put(test, RESULT.FAILURE);
-			} else if (r.result.equals(RESULT.ERROR)) {
-				this.errorTestsInOrder.add(test);
-				nameToResultsMap.put(test, RESULT.ERROR);
-			} else {
-				throw new RuntimeException("Unknown results: " + r);
-			}
-		}
-	}
-	
-	public List<String> getAllTests() {
-		return this.allTests;
-	}
-	
-	public OneTestExecResult getResult(String test) {
-		return this.singleRun.get(test);
-	}
-	
-	public boolean isTestPassed(String test) {
-		return this.passingTestsInOrder.contains(test);
-	}
-	
-	public boolean isTestFailed(String test) {
-		return this.failingTestsInOrder.contains(test);
-	}
-	
-	public boolean isTestError(String test) {
-		return this.errorTestsInOrder.contains(test);
-	}
-	
-	public Map<String, RESULT> getNameToResultsMap() {
-	    return nameToResultsMap;
-	}
+    /* The classified result after running all tests */
+    protected List<String> passingTestsInOrder = new LinkedList<String>();
+    protected List<String> failingTestsInOrder = new LinkedList<String>();
+    protected List<String> errorTestsInOrder = new LinkedList<String>();
+    protected Map<String, RESULT> nameToResultsMap = new HashMap<String, RESULT>();
 
-	@Override
-	public String toString() {
-		return this.singleRun.toString();
-	}
+    public TestExecResult(Map<String, OneTestExecResult> singleRun) {
+        this.singleRun = singleRun;
+        // classify each test
+        for (String test : this.singleRun.keySet()) {
+            allTests.add(test);
+            // check the results
+            OneTestExecResult r = this.singleRun.get(test);
+            if (r.result.equals(RESULT.PASS)) {
+                this.passingTestsInOrder.add(test);
+                nameToResultsMap.put(test, RESULT.PASS);
+            } else if (r.result.equals(RESULT.FAILURE)) {
+                this.failingTestsInOrder.add(test);
+                nameToResultsMap.put(test, RESULT.FAILURE);
+            } else if (r.result.equals(RESULT.ERROR)) {
+                this.errorTestsInOrder.add(test);
+                nameToResultsMap.put(test, RESULT.ERROR);
+            } else {
+                throw new RuntimeException("Unknown results: " + r);
+            }
+        }
+    }
+
+    public List<String> getAllTests() {
+        return this.allTests;
+    }
+
+    public OneTestExecResult getResult(String test) {
+        return this.singleRun.get(test);
+    }
+
+    public boolean isTestPassed(String test) {
+        return this.passingTestsInOrder.contains(test);
+    }
+
+    public boolean isTestFailed(String test) {
+        return this.failingTestsInOrder.contains(test);
+    }
+
+    public boolean isTestError(String test) {
+        return this.errorTestsInOrder.contains(test);
+    }
+
+    public Map<String, RESULT> getNameToResultsMap() {
+        return nameToResultsMap;
+    }
+
+    @Override
+    public String toString() {
+        return this.singleRun.toString();
+    }
 }
