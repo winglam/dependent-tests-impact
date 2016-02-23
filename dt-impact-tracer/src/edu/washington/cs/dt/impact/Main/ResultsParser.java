f23cbdfc7b3c59643150735d371e9353984c5aff
Aaron Chiu <achiu17@gmail.com>
2016-02-22 23:37:28 -0800

    Third commit

--- dt-impact-tracer/src/edu/washington/cs/dt/impact/Main/ResultsParser.java
+++ dt-impact-tracer/src/edu/washington/cs/dt/impact/Main/ResultsParser.java
@@ -10,7 +10,7 @@ import java.util.Scanner;
 public class ResultsParser {
 
     // name of line to get values from in Parallelization technique
-    public static final String PARALLELIZATION = "New order time:";
+    public static final String ORDER_TIME = "New order time:";
     // name of line to get values from in selection and prioritization
     public static final String APFD_VALUE = "APFD value:";
 
@@ -63,23 +63,73 @@ public class ResultsParser {
         return flagName;
     }
 
+    /*
+     * @param line will be a String of form ", prioritization, [...]"
+     *
+     * @return line without the commas and whitespace, i.e. "prioritization"
+     */
+    private static String parseArgs(String line) {
+        int delimiterIndex = line.indexOf(",", 1);
+        // -1 if last argument..takes on form: "-testType, orig]"
+        if (delimiterIndex == -1) {
+            delimiterIndex = line.indexOf("]");
+        }
+        return line.substring(1, delimiterIndex).trim();
+
+    }
+
+    /*
+     * a private method to generate a line of LaTeX needed for figure 17
+     *
+     * @param projectName the name of the project
+     *
+     * @param values the ordered values that need to be written
+     *
+     *
+     * format: Crystal & 0.026 & 0.001 & 0.000 & 0.000 \\
+     */
+    private static String generate17(String projectName, double[] values) {
+        String result = projectName;
+        for (double val : values) {
+            result += " & " + val;
+        }
+        result += " \\\\"; // "\\"
+        return result;
+    }
+
+    /*
+     * a private method to generate a line of LaTeX needed for figure 18
+     *
+     * @param projectName the name of the project
+     *
+     * @param percentages ordered percentages needed
+     *
+     * @param values the ordered values that need to be written
+     *
+     * format: Crystal & 5.4\%\pa & 1.3\%\pa & 0.000 & 0.000 & 0.008 & 0.015 & 0.036 & 0.000 \\
+     */
+    private static String generate18(String projectName, double[] percentages, double[] values) {
+        String result = projectName + "       ";
+        for (double percent : percentages) {
+            result += " & " + percent + "\\%\\pa"; // "\%\pa"
+        }
+        for (double val : values) {
+            result += " & " + val;
+        }
+        result += " \\\\"; // "\\"
+        return result;
+    }
+
     /**
      * @param args
      */
     public static void main(String[] args) {
         List<String> argsList = new ArrayList<String>(Arrays.asList(args));
 
-        String techniqueName = getArgName(argsList, "-technique");
-
         String directoryName = getArgName(argsList, "-directory");
 
-        String coverageName = getArgName(argsList, "-coverage");
-
-        String orderName = getArgName(argsList, "-order");
-
-        String projectName = getArgName(argsList, "-project");
-
-        String testType = getArgName(argsList, "-testType");
+        // open files with LaTeX template for figures 17, 18, and 19
+        // TODO *****************
 
         File directory = new File(directoryName);
         File[] fList = directory.listFiles();
@@ -90,6 +140,34 @@ public class ResultsParser {
                 // figure out how to generate the LaTeX needed for the
                 // diagrams...see Constants.java
 
+                // parseFile returns String of form ", prioritization, [...]"
+                // parseArgs returns "prioritization"
+
+                String techniqueName = parseFile(file, "-technique");
+                techniqueName = parseArgs(techniqueName);
+
+                String coverageName = parseFile(file, "-coverage");
+                coverageName = parseArgs(coverageName);
+
+                String orderName = parseFile(file, "-order");
+                orderName = parseArgs(orderName);
+
+                String projectName = parseFile(file, "-project");
+                projectName = parseArgs(projectName);
+
+                String testType = parseFile(file, "-testType");
+                testType = parseArgs(testType);
+
+                if (techniqueName.equals("parallelization")) {
+                    String order_time = parseFile(file, ORDER_TIME);
+
+                } else if (techniqueName.equals("selection")) { // selection techinque
+                    String apfd_value = parseFile(file, APFD_VALUE);
+
+                } else { // has to be prioritization or garbage value
+
+                }
+
             }
         }
 
@@ -97,6 +175,14 @@ public class ResultsParser {
     }
 
 }
+
+/*
+ * template for LaTeX generation for figure 17
+ * Crystal & 0.026 & 0.001 & 0.000 & 0.000 \\
+ *
+ * for figure 18
+ * Crystal & 5.4\%\pa & 1.3\%\pa & 0.000 & 0.000 & 0.008 & 0.015 & 0.036 & 0.000 \\
+ */
 
 // The tables needed to be generated and the type of args they require
 
@@ -114,56 +200,3 @@ public class ResultsParser {
  * S6 Function Number of uncovered elements tests cover
  */
 
-// LaTeX stuff needed to generate diagrams for figure 17 and 18
-
-/*******************
- * FIGURE 17********** \begin{figure} \smaller \centering
- * \begin{tabular}{|l|c|c|c|c|} \hline \textbf{Subject program} & T3 & T4 & T5 &
- * T7 \\ \hline \multicolumn{5}{|l|}{} \\
- * \multicolumn{5}{|l|}{\textbf{Human-written test suites}} \\ \hline Crystal &
- * 0.026 & 0.001 & 0.000 & 0.000 \\ JFreechart & 0.001 & 0.000 & 0.000 & 0.000
- * \\ \jt & 0.000 & 0.016 & 0.008 & 0.004 \\ Synoptic & 0.000 & 0.000 & 0.000 &
- * 0.000 \\ XML Security & 0.000 & 0.000 & 0.000 & 0.000 \\ \hline
- * \multicolumn{5}{|l|}{} \\
- * \multicolumn{5}{|l|}{\textbf{Automatically-generated test suites}} \\ \hline
- * Crystal & 0.011 & 0.075 & 0.000 & 0.067 \\ JFreechart & 0.000 & 0.000 & 0.006
- * & 0.000 \\ \jt & 0.014 & 0.063 & 0.000 & 0.040 \\ Synoptic & 0.000 & 0.002 &
- * 0.000 & 0.001 \\ XML Security & 0.120 & 0.164 & 0.118 & 0.203 \\ \hline
- * \end{tabular} \caption{ The \prionum \dependenceaware test prioritization
- * algorithms may choose a different test order than the original
- * prioritization. This table shows the decrease in the APFD value (a metric of
- * prioritization quality) between the original unsound prioritization algorithm
- * and the sound \dependenceaware prioritization algorithm.}
- * \label{tab:enhancedprioresult} \end{figure} %% LocalWords: T3 T4 T7
- * JFreechart
- */
-
-/******
- * FIGURE 18********** % Real asterisk \newcommand{\ra}{*$\!\!$} % Phantom
- * asterisk \newcommand{\pa}{\phantom{\ra}} \begin{figure} \smaller \centering
- * \setlength{\tabcolsep}{.5\tabcolsep} \begin{tabular}{|l|c|r|c|c|c|c|c|c|}
- * \hline \textbf{Subject} & \multicolumn{2}{c|}{Run time} &
- * \multicolumn{6}{c|}{APFD} \\ \textbf{program} & S1--S3 & S4--S6 & S1 & S2 &
- * S3 & S4 & S5 & S6 \\ \hline \multicolumn{9}{|l|}{} \\
- * \multicolumn{9}{|l|}{\textbf{Human-written test suites}} \\ \hline Crystal &
- * 5.4\%\pa & 1.3\%\pa & 0.000 & 0.000 & 0.008 & 0.015 & 0.036 & 0.000 \\
- * JFreechart & 0.7\%\pa & 0.6\%\pa & 0.001 & 0.001 & 0.000 & 0.000 & 0.004 &
- * 0.003 \\ \jt & 1.8\%\pa & 11.4\%\pa & 0.013 & 0.000 & 0.001 & 0.000 & 0.008 &
- * 0.037 \\ Synoptic & 2.4\%\pa & 0.0\%\pa & 0.000 & 0.033 & 0.023 & 0.000 &
- * 0.000 & 0.048 \\ XML Security & 0.0\%\pa & 0.0\%\pa & 0.000 & 0.000 & 0.000 &
- * 0.000 & 0.000 & 0.000 \\ \hline \multicolumn{9}{|l|}{} \\
- * \multicolumn{9}{|l|}{\textbf{Automatically-generated test suites}} \\ \hline
- * Crystal & 0.0\%\ra & 0.7\%\ra & 0.000 & 0.561 & 0.781 & 0.006 & 0.851 & 0.900
- * \\ JFreechart & 3.3\%\pa & 2.0\%\pa & 0.000 & 0.000 & 0.016 & 0.000 & 0.000 &
- * 0.008 \\ \jt & 7.2\%\pa & 7.3\%\pa & 0.000 & 0.018 & 0.074 & 0.000 & 0.000 &
- * 0.056 \\ Synoptic & 0.0\%\pa & 0.0\%\pa & 0.001 & 0.007 & 0.006 & 0.006 &
- * 0.006 & 0.000 \\ XML Security & 8.0\%\pa & 10.9\%\pa & 0.000 & 0.075 & 0.102
- * & 0.000 & 0.047 & 0.055 \\ \hline \end{tabular} \caption{ The \selnum
- * \dependenceaware test selection algorithms may reorder the selected test
- * suite, increase the number of selected tests (the size and run time of the
- * resulting suite), or do both. This table shows the increase in the run time
- * and the decrease in APFD\@. An asterisk indicates a value for algorithm S1 or
- * S4; algorithms S2, S3, S5, and S6 timed out and did not select a subsuite for
- * Crystal's automatically-generated test suite. } \label{tab:enhancedselresult}
- * \end{figure} %% LocalWords: S1 S3 S4 S6 S2 S5 JFreechart
- */
