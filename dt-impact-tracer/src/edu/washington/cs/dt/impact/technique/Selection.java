8131610def45422c7cfad99860ad120777d6b987
Wing Lam <wing.c.lam@gmail.com>
2016-03-09 18:30:00 -0600

    Selection should now support getting APFD

--- dt-impact-tracer/src/edu/washington/cs/dt/impact/technique/Selection.java
+++ dt-impact-tracer/src/edu/washington/cs/dt/impact/technique/Selection.java
@@ -43,7 +43,7 @@ public class Selection extends Test {
      *            regarding which test depends on which
      */
     public Selection(ORDER order, String outputFileName, File inputTestFolder, COVERAGE coverage, File selectionOutput1,
-            File selectionOutput2, File origOrder, File dependentTestsFile) {
+            File selectionOutput2, File origOrder, File dependentTestsFile, boolean getCoverage) {
         super(inputTestFolder, coverage, dependentTestsFile);
 
         Set<String> changedCoverage = findCoverage(selectionOutput1, selectionOutput2, coverage);
@@ -77,7 +77,7 @@ public class Selection extends Test {
                         + " Compatible orders are: absolute, relative, random and original.");
                 System.exit(0);
             }
-            orderObj = new Standard(outputFileName, methodList);
+            orderObj = new Standard(outputFileName, methodList, getCoverage, allCoverageLines);
         }
     }
 
