275f7a78414a08158a6bc2b318647403a33e3100
Aaron Chiu <achiu17@gmail.com>
2016-03-09 19:17:04 -0800

    Script to output figures automatically

---
+++ experiments/figureGenerator.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Running random-runner script"
+
+sh ./random-runner.sh
+
+echo "Running prioritization-runner script"
+
+sh ./prioritization-runner.sh
+
+echo "Running selection-runner script"
+
+sh ./selection-runner.sh
+
