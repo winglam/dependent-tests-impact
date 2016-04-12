b0f2019fa1aefa366a276972fba10869185be626
Aaron Chiu <achiu17@gmail.com>
2016-04-12 00:10:02 -0700

    Uncommented lines in scripts that allowed them to run faster

--- experiments/prioritization-runner.sh
+++ experiments/prioritization-runner.sh
@@ -15,14 +15,12 @@ while [ "$index" -lt "$count" ]; do
     echo 'Generating sootTestOutput'
   	java -cp ${sootCP[$index]} edu.washington.cs.dt.main.ImpactMain -inputTests ${experiments[$index]}-$k-order
 
-    java -cp ${experimentsCP[$index]} edu.washington.cs.dt.impact.Main.OneConfigurationRunner -technique prioritization -coverage statement -order original -origOrder ${experiments[$index]}-$k-order -testInputDir sootTestOutput -filesToDelete ${experiments[$index]}-env-files -project ${experiments[$index]} -testType $k -outputDir ../${priorDir} -timesToRun ${medianTimes} -getCoverage
-
     for i in "${coverages[@]}"; do
       for j in "${priorOrders[@]}"; do
         echo 'Running prioritization without resolveDependences and with dependentTestFile'
-        #java -cp ${experimentsCP[$index]} edu.washington.cs.dt.impact.Main.OneConfigurationRunner -technique prioritization -coverage $i -order $j -origOrder ${experiments[$index]}-$k-order -testInputDir sootTestOutput -filesToDelete ${experiments[$index]}-env-files -getCoverage -project ${experiments[$index]} -testType $k -outputDir ../${priorDir} -dependentTestFile ../ -timesToRun ${medianTimes}
+        java -cp ${experimentsCP[$index]} edu.washington.cs.dt.impact.Main.OneConfigurationRunner -technique prioritization -coverage $i -order $j -origOrder ${experiments[$index]}-$k-order -testInputDir sootTestOutput -filesToDelete ${experiments[$index]}-env-files -getCoverage -project ${experiments[$index]} -testType $k -outputDir ../${priorDir} -dependentTestFile ../ -timesToRun ${medianTimes}
         echo 'Running prioritization without resolveDependences and without dependentTestFile'
-        #java -cp ${experimentsCP[$index]} edu.washington.cs.dt.impact.Main.OneConfigurationRunner -technique prioritization -coverage $i -order $j -origOrder ${experiments[$index]}-$k-order -testInputDir sootTestOutput -filesToDelete ${experiments[$index]}-env-files -getCoverage -project ${experiments[$index]} -testType $k -outputDir ../${priorDir} -timesToRun ${medianTimes}
+        java -cp ${experimentsCP[$index]} edu.washington.cs.dt.impact.Main.OneConfigurationRunner -technique prioritization -coverage $i -order $j -origOrder ${experiments[$index]}-$k-order -testInputDir sootTestOutput -filesToDelete ${experiments[$index]}-env-files -getCoverage -project ${experiments[$index]} -testType $k -outputDir ../${priorDir} -timesToRun ${medianTimes}
       done
     done
     clearTemp ${experiments[$index]} $k
