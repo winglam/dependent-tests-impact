2cf4663b8060bfa56d878d5a711dfd6da527e758
Wing Lam <wing.c.lam@gmail.com>
2016-06-18 22:28:24 +0100

    Changing scripts to only run prioritization for figure 7

--- experiments/parallelization-runner.sh
+++ experiments/parallelization-runner.sh
@@ -7,8 +7,8 @@ while [ "$index" -lt "$count" ]; do
   cd ${directories[$index]}
 
   for j in "${testTypes[@]}"; do
-    #enhancedInstrumentFiles ${instrumentationCP[$index]}
-    instrumentFiles ${experimentsCP[$index]}
+    enhancedInstrumentFiles ${instrumentationCP[$index]}
+    #instrumentFiles ${experimentsCP[$index]}
 
     echo 'Generating sootTestOutput'
   	java -cp ${sootCP[$index]} edu.washington.cs.dt.main.ImpactMain -inputTests ${experiments[$index]}-$j-order
--- experiments/prioritization-runner.sh
+++ experiments/prioritization-runner.sh
@@ -10,8 +10,8 @@ while [ "$index" -lt "$count" ]; do
   cd ${directories[$index]}
 
   for k in "${testTypes[@]}"; do
-    #enhancedInstrumentFiles ${instrumentationCP[$index]}
-    instrumentFiles ${experimentsCP[$index]}
+    enhancedInstrumentFiles ${instrumentationCP[$index]}
+    #instrumentFiles ${experimentsCP[$index]}
 
     echo 'Generating sootTestOutput'
   	java -cp ${sootCP[$index]} edu.washington.cs.dt.main.ImpactMain -inputTests ${experiments[$index]}-$k-order
