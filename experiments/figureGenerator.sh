c8fe568e56fed3750cf2acaf3664d797a7b06cc9
Wing Lam <wing.c.lam@gmail.com>
2016-09-25 00:01:57 -0500

    Setting scripts up to run prioritization and selection.

--- experiments/figureGenerator.sh
+++ experiments/figureGenerator.sh
@@ -7,7 +7,7 @@ function runPrioritization() {
     mkdir ${priorDir}
 
     ./prioritization-runner.sh
-    java -cp ${impactJarCP} edu.washington.cs.dt.impact.Main.EnhancedResultsFigureGenerator -directory ${priorDir} -outputDirectory ${priorDir} -allowNegatives
+    #java -cp ${impactJarCP} edu.washington.cs.dt.impact.Main.EnhancedResultsFigureGenerator -directory ${priorDir} -outputDirectory ${priorDir} -allowNegatives
     java -cp ${impactJarCP} edu.washington.cs.dt.impact.Main.NumDependentTestsFigureGenerator -directory ${priorDir} -outputDirectory ${priorDir}
 }
 
@@ -16,7 +16,7 @@ function runSelection() {
     mkdir ${seleDir}
 
     ./selection-runner.sh
-    java -cp ${impactJarCP} edu.washington.cs.dt.impact.Main.EnhancedResultsFigureGenerator -directory ${seleDir} -outputDirectory ${seleDir} -allowNegatives
+    #java -cp ${impactJarCP} edu.washington.cs.dt.impact.Main.EnhancedResultsFigureGenerator -directory ${seleDir} -outputDirectory ${seleDir} -allowNegatives
     java -cp ${impactJarCP} edu.washington.cs.dt.impact.Main.NumDependentTestsFigureGenerator -directory ${seleDir} -outputDirectory ${seleDir}
 }
 
@@ -32,13 +32,13 @@ function runParallelization() {
 #echo "Running random-runner script"
 #./random-runner.sh
 
-#echo "Running prioritization-runner script"
-#runPrioritization
+echo "Running prioritization-runner script"
+runPrioritization
 
-#echo "Running selection-runner script"
-#runSelection
+echo "Running selection-runner script"
+runSelection
 
-echo "Running paralleization-runner script"
-runParallelization
+#echo "Running paralleization-runner script"
+#runParallelization
 
 echo "Script has finished running."
