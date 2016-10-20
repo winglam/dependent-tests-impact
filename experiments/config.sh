d4527d1d0cf71b48357b5830367bcc51be1defb9
Wing Lam <wing.c.lam@gmail.com>
2016-10-19 23:59:00 -0500

    Increasing median time to get more accurate enhanced results.

--- experiments/config.sh
+++ experiments/config.sh
@@ -2,7 +2,7 @@
 # file.
 
 # Number of times to run the test order before taking the median
-medianTimes=1
+medianTimes=3
 # Number of times to randomize the test order when calculating the
 # precomputed dependences
 randomTimes=100
