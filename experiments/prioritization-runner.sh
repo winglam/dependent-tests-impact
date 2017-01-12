66547e2dc685f2f50534032189c43300b7a1cef8
Wing Lam <winglam@buffalo.cs.washington.edu>
2017-01-11 16:52:05 -0800

    Fixed bug in script where names were separated by space.

--- experiments/prioritization-runner.sh
+++ experiments/prioritization-runner.sh
@@ -15,16 +15,16 @@ while [ "$index" -lt "$count" ]; do
     #echo 'Generating sootTestOutput'
     #java -cp ${sootCP[$index]} edu.washington.cs.dt.main.ImpactMain -inputTests ${experiments[$index]}-$k-order
     #mv sootTestOutput sootTestOutput-$k
-    java -cp ${experimentsCP[$index]} edu.washington.cs.dt.impact.Main.OneConfigurationRunner -technique prioritization -coverage statement -order original -origOrder ${experiments[$index]}-$k-order -testInputDir sootTestOutput-$k -filesToDelete ${experiments[$index]}-env-files -project ${experimentsName[$index]} -testType $k -outputDir ${initialDir}/${priorDir} -timesToRun ${medianTimes} -getCoverage
+    java -cp ${experimentsCP[$index]} edu.washington.cs.dt.impact.Main.OneConfigurationRunner -technique prioritization -coverage statement -order original -origOrder ${experiments[$index]}-$k-order -testInputDir sootTestOutput-$k -filesToDelete ${experiments[$index]}-env-files -project "${experimentsName[$index]}" -testType $k -outputDir ${initialDir}/${priorDir} -timesToRun ${medianTimes} -getCoverage
 
     for i in "${coverages[@]}"; do
       for j in "${priorOrders[@]}"; do
         #echo 'Running prioritization without resolveDependences and with dependentTestFile'
-        #java -cp ${experimentsCP[$index]} edu.washington.cs.dt.impact.Main.OneConfigurationRunner -technique prioritization -coverage $i -order $j -origOrder ${experiments[$index]}-$k-order -testInputDir sootTestOutput-$k -filesToDelete ${experiments[$index]}-env-files -getCoverage -project ${experimentsName[$index]} -testType $k -outputDir ${initialDir}/${priorDir} -dependentTestFile ${initialDir}/ -timesToRun ${medianTimes}
+        #java -cp ${experimentsCP[$index]} edu.washington.cs.dt.impact.Main.OneConfigurationRunner -technique prioritization -coverage $i -order $j -origOrder ${experiments[$index]}-$k-order -testInputDir sootTestOutput-$k -filesToDelete ${experiments[$index]}-env-files -getCoverage -project "${experimentsName[$index]}" -testType $k -outputDir ${initialDir}/${priorDir} -dependentTestFile ${initialDir}/ -timesToRun ${medianTimes}
         #echo 'Running prioritization without resolveDependences and without dependentTestFile'
-        #java -cp ${experimentsCP[$index]} edu.washington.cs.dt.impact.Main.OneConfigurationRunner -technique prioritization -coverage $i -order $j -origOrder ${experiments[$index]}-$k-order -testInputDir sootTestOutput-$k -filesToDelete ${experiments[$index]}-env-files -getCoverage -project ${experimentsName[$index]} -testType $k -outputDir ${initialDir}/${priorDir} -timesToRun ${medianTimes}
+        #java -cp ${experimentsCP[$index]} edu.washington.cs.dt.impact.Main.OneConfigurationRunner -technique prioritization -coverage $i -order $j -origOrder ${experiments[$index]}-$k-order -testInputDir sootTestOutput-$k -filesToDelete ${experiments[$index]}-env-files -getCoverage -project "${experimentsName[$index]}" -testType $k -outputDir ${initialDir}/${priorDir} -timesToRun ${medianTimes}
         echo 'Running prioritization with resolveDependences and without dependentTestFile'
-        java -cp ${experimentsCP[$index]} edu.washington.cs.dt.impact.Main.OneConfigurationRunner -technique prioritization -coverage $i -order $j -origOrder ${experiments[$index]}-$k-order -testInputDir sootTestOutput-$k -filesToDelete ${experiments[$index]}-env-files -getCoverage -project ${experimentsName[$index]} -testType $k -outputDir ${initialDir}/${priorDir} -timesToRun ${medianTimes} -resolveDependences
+        java -cp ${experimentsCP[$index]} edu.washington.cs.dt.impact.Main.OneConfigurationRunner -technique prioritization -coverage $i -order $j -origOrder ${experiments[$index]}-$k-order -testInputDir sootTestOutput-$k -filesToDelete ${experiments[$index]}-env-files -getCoverage -project "${experimentsName[$index]}" -testType $k -outputDir ${initialDir}/${priorDir} -timesToRun ${medianTimes} -resolveDependences
       done
     done
     clearTemp ${experiments[$index]} $k
