f8ced510daaa73d4a42d0c10c03a30a677ddad3f
Wing Lam <wing.c.lam@gmail.com>
2017-02-11 16:17:42 -0800

    Updating enhanced scripts to quickly generate ambari-server results.

--- experiments/newExperimentsParallelizationRunner.sh
+++ experiments/newExperimentsParallelizationRunner.sh
@@ -11,7 +11,7 @@ while [ "$index" -lt "$count" ]; do
 
   for j in "${newExpTestTypes[@]}"; do
     #echo 'Running prioritization for original order'
-    java -Xms1g -Xmx2g -cp ${newExpCP[$index]} edu.washington.cs.dt.impact.Main.OneConfigurationRunner -technique prioritization -coverage statement -order original -origOrder ${newExperiments[$index]}-$j-order -testInputDir sootTestOutput-$j -filesToDelete ${newExperiments[$index]}-env-files -project "${newExperimentsName[$index]}" -testType $j -outputDir ${initialDir}/${paraDir} -timesToRun ${medianTimes} -getCoverage
+    #java -Xms1g -Xmx2g -cp ${newExpCP[$index]} edu.washington.cs.dt.impact.Main.OneConfigurationRunner -technique prioritization -coverage statement -order original -origOrder ${newExperiments[$index]}-$j-order -testInputDir sootTestOutput-$j -filesToDelete ${newExperiments[$index]}-env-files -project "${newExperimentsName[$index]}" -testType $j -outputDir ${initialDir}/${paraDir} -timesToRun ${medianTimes} -getCoverage
 
     runNewExpParallelizationOneConfigurationRunner ${newExperiments[$index]} ${newExpCP[$index]} $j "${newExperimentsName[$index]}"
     clearTemp ${newExperiments[$index]} $j
