9da8c6df0af7c8f36afd817dda4bf237ae75253b
Wing Lam <wing.c.lam@gmail.com>
2017-03-25 19:49:19 -0700

    Fixed bug in script where an argument was missing in enhanced new experiments parallelization.

--- experiments/newExperimentsParallelizationRunner.sh
+++ experiments/newExperimentsParallelizationRunner.sh
@@ -13,7 +13,7 @@ while [ "$index" -lt "$count" ]; do
     #echo 'Running prioritization for original order'
     #java -Xms1g -Xmx2g -cp ${newExpCP[$index]} edu.washington.cs.dt.impact.Main.OneConfigurationRunner -technique prioritization -coverage statement -order original -origOrder ${newExperiments[$index]}-$j-order -testInputDir sootTestOutput-$j -filesToDelete ${newExperiments[$index]}-env-files -project "${newExperimentsName[$index]}" -testType $j -outputDir ${initialDir}/${paraDir} -timesToRun ${medianTimes} -getCoverage
 
-    runNewExpParallelizationOneConfigurationRunner ${newExperiments[$index]} ${newExpCP[$index]} $j "${newExperimentsName[$index]}"
+    runNewExpParallelizationOneConfigurationRunner ${newExperiments[$index]} ${newExpCP[$index]} $j "${newExperimentsName[$index]}" ${initialDir}/${newExpDirectories[$index]}/
     clearTemp ${newExperiments[$index]} $j
   done
 
