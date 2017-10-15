5f821c21250f97eadbf15a40de59726ad9afb50d
Reed Oei <oei.reed@gmail.com>
2017-10-14 19:25:31 -0500

    Configured dynoptic for automated setup scripts/created script.

---
+++ experiments/scripts/subj-scripts/dynoptic.sh
@@ -0,0 +1,19 @@
+# The root directory for the old subject
+export DT_SUBJ_ROOT=/home/user/dependent-tests-impact/experiments/dynoptic
+# Directory for where all of the old subject's information is stored.
+export DT_SUBJ=$DT_SUBJ_ROOT
+# Directory containing the old subject's src directory.
+export DT_SUBJ_SRC=$DT_SUBJ_ROOT
+
+# The root directory for the new subject
+export NEW_DT_SUBJ_ROOT=/home/user/dependent-tests-impact/experiments/dynoptic-ea407ba0a750
+# Directory for where all of the new subject's information is stored.
+export NEW_DT_SUBJ=$NEW_DT_SUBJ_ROOT
+# Directory containing the new subject's src directory.
+export NEW_DT_SUBJ_SRC=$NEW_DT_SUBJ_ROOT
+
+# The name of the subject you wish to add (e.g., xml_security)
+export SUBJ_NAME=dynoptic
+# The name of the subject you want to be displayed in the paper (e.g., XML Security)
+export SUBJ_NAME_FORMAL=Dynoptic
+
