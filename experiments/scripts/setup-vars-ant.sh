dc8a4e49d19fce1e0525fec2c260e440467b28f2
Reed Oei <oei.reed@gmail.com>
2017-10-14 19:11:16 -0500

    Created setup scripts for projects.

---
+++ experiments/scripts/setup-vars-ant.sh
@@ -0,0 +1,43 @@
+# Root directory for our tools and results
+export DT_ROOT=/home/user/dependent-tests-impact/experiments
+
+# Directory for all the jar files containing the DT-impact tools and its dependencies
+export DT_TOOLS=$DT_ROOT/impact-tools/*
+
+# Directory for libraries of the old subject
+export DT_LIBS=$DT_SUBJ/lib/*
+# Compiled class files of the old subject
+export DT_CLASS=$DT_SUBJ/bin
+# Compiled automatically-generated test files of the old subject
+export DT_RANDOOP=$DT_SUBJ/bin/randoop
+# Compiled human-written test files of the old subject
+export DT_TESTS=$DT_SUBJ/bin
+
+# Directory for libraries of the new subject
+export NEW_DT_LIBS=$NEW_DT_SUBJ/lib/*
+# Compiled class files of the new subject
+export NEW_DT_CLASS=$NEW_DT_SUBJ/bin
+# Compiled automatically-generated test files of the new subject
+export NEW_DT_RANDOOP=$NEW_DT_SUBJ/bin/randoop
+# Compiled human-written test files of the new subject
+export NEW_DT_TESTS=$NEW_DT_SUBJ/bin
+
+# Directory for all the jar files containing the DT-impact tools and its dependencies
+export PRIO_RESULTS=$DT_ROOT/prioritization-results
+export SELE_RESULTS=$DT_ROOT/selection-results
+export PARA_RESULTS=$DT_ROOT/parallelization-results
+
+# Directory for all the scripts used to generate the figures in our paper
+export DT_SCRIPTS=$DT_ROOT/scripts
+# Directory for all the hardcoded data related to our subjects
+export DT_DATA=$DT_SCRIPTS/data
+
+# Directory for all the jar files containing the DT-impact tools and its dependencies
+export PRIO_DT_LISTS=$DT_DATA/prioritization-dt-list
+export SELE_DT_LISTS=$DT_DATA/selection-dt-list
+export PARA_DT_LISTS=$DT_DATA/parallelization-dt-list
+
+# Files containing the minimum number of dependent tests a subject has
+export AUTO_MIN_DTS=$DT_DATA/AUTO_MIN_DTs.txt
+export ORIG_MIN_DTS=$DT_DATA/ORIG_MIN_DTs.txt
+
--- experiments/scripts/subj-scripts/jodatime.sh
+++ experiments/scripts/subj-scripts/jodatime.sh
@@ -1,19 +1,19 @@
 # The root directory for the old subject
-export DT_SUBJ_ROOT=/home/user/dependent-tests-impact/experiments/ambari
+export DT_SUBJ_ROOT=/home/user/dependent-tests-impact/experiments/jodatime-b609d7d66d
 # Directory for where all of the old subject's information is stored.
-export DT_SUBJ=$DT_SUBJ_ROOT/ambari-server/target
+export DT_SUBJ=$DT_SUBJ_ROOT
 # Directory containing the old subject's src directory.
-export DT_SUBJ_SRC=$DT_SUBJ_ROOT/ambari-server
+export DT_SUBJ_SRC=$DT_SUBJ_ROOT
 
 # The root directory for the new subject
-export NEW_DT_SUBJ_ROOT=/home/user/dependent-tests-impact/experiments/ambari-new
+export NEW_DT_SUBJ_ROOT=/home/user/dependent-tests-impact/experiments/jodatime-d6791cb5f9
 # Directory for where all of the new subject's information is stored.
-export NEW_DT_SUBJ=$NEW_DT_SUBJ_ROOT/ambari-server/target
+export NEW_DT_SUBJ=$NEW_DT_SUBJ_ROOT
 # Directory containing the new subject's src directory.
-export NEW_DT_SUBJ_SRC=$NEW_DT_SUBJ_ROOT/ambari-server
+export NEW_DT_SUBJ_SRC=$NEW_DT_SUBJ_ROOT
 
 # The name of the subject you wish to add (e.g., xml_security)
-export SUBJ_NAME=ambari-server
+export SUBJ_NAME=jodatime
 # The name of the subject you want to be displayed in the paper (e.g., XML Security)
-export SUBJ_NAME_FORMAL=Ambari-Server
+export SUBJ_NAME_FORMAL=Joda-Time
 
