48ea6fbcd7d66c81aae67a2f276d0439c96db6a7
Reed Oei <oei.reed@gmail.com>
2017-10-16 18:07:15 -0500

    Changed dynoptic setup script so that copying libs was not necessary.

--- experiments/scripts/subj-scripts/dynoptic.sh
+++ experiments/scripts/subj-scripts/dynoptic.sh
@@ -10,3 +10,10 @@ export SUBJ_NAME=dynoptic
 export SUBJ_NAME_FORMAL=Dynoptic
 
 source ../setup-vars-ant.sh
+
+# Override some of the variables we just set because dynoptic setup is a little different.
+export DT_CLASS=$DT_SUBJ/bin/:/home/user/dependent-tests-impact/experiments/synoptic/bin/:/home/user/dependent-tests-impact/experiments/daikonizer/bin/
+export NEW_DT_CLASS=$NEW_DT_SUBJ/bin/:/home/user/dependent-tests-impact/experiments/synoptic/bin/:/home/user/dependent-tests-impact/experiments/daikonizer/bin/
+
+export DT_LIBS=/home/user/dependent-tests-impact/experiments/synoptic/lib/*
+export NEW_DT_LIBS=/home/user/dependent-tests-impact/experiments/synoptic/lib/*
