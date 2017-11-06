97daaeb071962c6671706158be65f24526336f37
Reed Oei <oei.reed@gmail.com>
2017-11-06 17:24:15 -0600

    Updated comments to reflect guidelines changes.

--- experiments/scripts/compile-subj.sh
+++ experiments/scripts/compile-subj.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
+# Automates the "Instructions for compiling subjects" section.
+
 # 1. Compile the old subject.
 cd $DT_SUBJ_ROOT
 mvn compile
 mvn test-compile
 
-# 2. Gather the dependencies of the old subject. 
+# 2. Gather the dependencies of the old subject.
 mvn install -fn -DskipTests dependency:copy-dependencies
 
-# 1. Compile the new subject.
+# 3. Compile the new subject.
 cd $NEW_DT_SUBJ_ROOT
 mvn compile
 mvn test-compile
 
-# 2. Gather the dependencies of the new subject. 
+# 4. Gather the dependencies of the new subject.
 mvn install -fn -DskipTests dependency:copy-dependencies
--- experiments/scripts/setup-prio.sh
+++ experiments/scripts/setup-prio.sh
@@ -2,19 +2,19 @@
 
 # Runs commands for "Instructions to setup a subject for test prioritization" section.
 
-# 3. Find the human-written tests in the old subject.
+# 1. Find the human-written tests in the old subject.
 cd $DT_SUBJ
 java -cp $DT_TOOLS:$DT_LIBS:$DT_CLASS:$DT_TESTS: edu.washington.cs.dt.tools.UnitTestFinder \
   --pathOrJarFile $DT_TESTS --junit3and4=true
 mv allunittests.txt $SUBJ_NAME-orig-order
 
-# 4. Instrument the source and test files.
+# 2. Instrument the source and test files.
 java -cp $DT_TOOLS:$DT_LIBS:$DT_CLASS:$DT_TESTS:$JAVA_HOME/jre/lib/*: edu.washington.cs.dt.impact.Main.InstrumentationMain \
   -inputDir $DT_TESTS
 java -cp $DT_TOOLS:$DT_LIBS:$DT_CLASS:$JAVA_HOME/jre/lib/*: edu.washington.cs.dt.impact.Main.InstrumentationMain \
   -inputDir $DT_CLASS
 
-# 5. Run the instrumented tests.
+# 3. Run the instrumented tests.
 java -cp $DT_TOOLS:$DT_LIBS:$DT_SUBJ/sootOutput/: edu.washington.cs.dt.main.ImpactMain -inputTests $SUBJ_NAME-orig-order
 mv sootTestOutput/ sootTestOutput-orig
 rm -rf sootOutput/
