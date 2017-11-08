63e33a6f0da03ab47c2ba008613207cf1236d383
Henry Wu <henrywu847@gmail.com>
2017-11-07 21:20:02 -0800

    adding results for activemq-camel

--- experiments/scripts/subj-constants.sh
+++ experiments/scripts/subj-constants.sh
@@ -2,95 +2,103 @@
 rootDir=$1
 # The name of the subject you wish to add (e.g., xml_security).
 expName=(
-#  crystal
-#  jfreechart
-#  jodatime
-#  synoptic
-#  xml_security
-#  ambari-server
-#  zeppelin-zengine
-  zeppelin-server
+    #crystal
+    #jfreechart
+    #jodatime
+    #synoptic
+    #xml_security
+    ambari-server
+    #zeppelin-zengine
+    #zeppelin-server
+    #activemqCamel
 )
 # Directory for where all of the old subject's information is stored.
 expDirectories=(
-#  ${rootDir}/crystalvc
-#  ${rootDir}/jfreechart-1.0.15
-#  ${rootDir}/jodatime-b609d7d66d
-#  ${rootDir}/dynoptic
-#  ${rootDir}/xml-security-orig-v1
-#  ${rootDir}/ambari/ambari-server/target
-#  ${rootDir}/zeppelin/zeppelin-zengine/target
-  ${rootDir}/zeppelin/zeppelin-server/target
+    #${rootDir}/crystalvc
+    #${rootDir}/jfreechart-1.0.15
+    #${rootDir}/jodatime-b609d7d66d
+    #${rootDir}/dynoptic
+    #${rootDir}/xml-security-orig-v1
+    ${rootDir}/ambari/ambari-server/target
+    #${rootDir}/zeppelin/zeppelin-zengine/target
+    #${rootDir}/zeppelin/zeppelin-server/target
+    #/home/user/dependent-tests-impact/experiments/activemq-old/activemq-camel/target
 )
 # Classpath to run the old subject's automatically-generated and human-written tests.
 expCP=(
-#  ${rootDir}/impact-tools/*:${rootDir}/crystalvc/bin/:${rootDir}/crystalvc/lib/*
-#  ${rootDir}/impact-tools/*:${rootDir}/jfreechart-1.0.15/bin/:${rootDir}/jfreechart-1.0.15/lib/*
-#  ${rootDir}/impact-tools/*:${rootDir}/jodatime-b609d7d66d/bin/:${rootDir}/jodatime-b609d7d66d/resources/:${rootDir}/jodatime-b609d7d66d/lib/*
-#  ${rootDir}/impact-tools/*:${rootDir}/dynoptic/bin/:${rootDir}/synoptic/lib/*:${rootDir}/synoptic/bin/:${rootDir}/daikonizer/bin/
-#  ${rootDir}/impact-tools/*:${rootDir}/xml-security-orig-v1/bin/:${rootDir}/xml-security-commons/bin/:data/:${rootDir}/xml-security-commons/libs/*
-#  ${rootDir}/impact-tools/*:${rootDir}/ambari/ambari-server/target/classes/:${rootDir}/ambari/ambari-server/target/test-classes/:${rootDir}/ambari/ambari-server/target/dependency/*:${rootDir}/ambari/ambari-server/target/randoop/bin/:/usr/lib/jvm/java-7-oracle/jre/lib/*:
-#  ${rootDir}/impact-tools/*:${rootDir}/zeppelin/zeppelin-zengine/target/classes/:${rootDir}/zeppelin/zeppelin-zengine/target/test-classes/:${rootDir}/zeppelin/zeppelin-zengine/target/dependency/*:${rootDir}/zeppelin/zeppelin-zengine/target/randoop/bin/:/usr/lib/jvm/java-7-oracle/jre/lib/*:
-  ${rootDir}/impact-tools/*:${rootDir}/zeppelin/zeppelin-server/target/classes/:${rootDir}/zeppelin/zeppelin-server/target/test-classes/:${rootDir}/zeppelin/zeppelin-server/target/dependency/*:${rootDir}/zeppelin/zeppelin-server/target/randoop/bin/:/usr/lib/jvm/java-7-oracle/jre/lib/*:
+    #${rootDir}/impact-tools/*:${rootDir}/crystalvc/bin/:${rootDir}/crystalvc/lib/*
+    #${rootDir}/impact-tools/*:${rootDir}/jfreechart-1.0.15/bin/:${rootDir}/jfreechart-1.0.15/lib/*
+    #${rootDir}/impact-tools/*:${rootDir}/jodatime-b609d7d66d/bin/:${rootDir}/jodatime-b609d7d66d/resources/:${rootDir}/jodatime-b609d7d66d/lib/*
+    #${rootDir}/impact-tools/*:${rootDir}/dynoptic/bin/:${rootDir}/synoptic/lib/*:${rootDir}/synoptic/bin/:${rootDir}/daikonizer/bin/
+    #${rootDir}/impact-tools/*:${rootDir}/xml-security-orig-v1/bin/:${rootDir}/xml-security-commons/bin/:${rootDir}/xml-security-orig-v1/data/:${rootDir}/xml-security-commons/libs/*
+    ${rootDir}/impact-tools/*:${rootDir}/ambari/ambari-server/target/classes/:${rootDir}/ambari/ambari-server/target/test-classes/:${rootDir}/ambari/ambari-server/target/dependency/*:${rootDir}/ambari/ambari-server/target/randoop/bin/:/usr/lib/jvm/java-7-oracle/jre/lib/*:
+    #${rootDir}/impact-tools/*:${rootDir}/zeppelin/zeppelin-zengine/target/classes/:${rootDir}/zeppelin/zeppelin-zengine/target/test-classes/:${rootDir}/zeppelin/zeppelin-zengine/target/dependency/*:${rootDir}/zeppelin/zeppelin-zengine/target/randoop/bin/:/usr/lib/jvm/java-7-oracle/jre/lib/*:
+    #${rootDir}/impact-tools/*:${rootDir}/zeppelin/zeppelin-server/target/classes/:${rootDir}/zeppelin/zeppelin-server/target/test-classes/:${rootDir}/zeppelin/zeppelin-server/target/dependency/*:${rootDir}/zeppelin/zeppelin-server/target/randoop/bin/:/usr/lib/jvm/java-7-oracle/jre/lib/*:
+    #/home/user/dependent-tests-impact/experiments/impact-tools/*:/home/user/dependent-tests-impact/experiments/activemq-old/activemq-camel/target/dependency/*:/home/user/dependent-tests-impact/experiments/activemq-old/activemq-camel/target/classes:/home/user/dependent-tests-impact/experiments/activemq-old/activemq-camel/target/randoop/bin:/home/user/dependent-tests-impact/experiments/activemq-old/activemq-camel/target/test-classes:
 )
 # The name of the subject you want to be displayed in the paper (e.g., XML Security).
 expNameFormal=(
-#  "Crystal"
-#  "JFreechart"
-#  "Joda-Time"
-#  "Synoptic"
-#  "XML Security"
-#  "Ambari-Server"
-#  "Zeppelin-Zengine"
-  "Zeppelin-Server"
+    #"Crystal"
+    #"JFreechart"
+    #"Joda-Time"
+    #"Synoptic"
+    #"XML Security"
+    "Ambari-Server"
+    #"Zeppelin-Zengine"
+    #"Zeppelin-Server"
+    #"ActivemqCamel"
 )
 # Directory for where all of the new subject's information is stored.
 nextExpDirectories=(
-#  ${rootDir}/crystal
-#  ${rootDir}/jfreechart-1.0.16
-#  ${rootDir}/jodatime-d6791cb5f9
-#  ${rootDir}/dynoptic-ea407ba0a750
-#  ${rootDir}/xml-security-1_2_0
-#  ${rootDir}/ambari-new/ambari-server/target
-#  ${rootDir}/zeppelin-zengine-new/zeppelin-zengine/target
-  ${rootDir}/zeppelin-new/zeppelin-server/target
+    #${rootDir}/crystal
+    #${rootDir}/jfreechart-1.0.16
+    #${rootDir}/jodatime-d6791cb5f9
+    #${rootDir}/dynoptic-ea407ba0a750
+    #${rootDir}/xml-security-1_2_0
+    ${rootDir}/ambari-new/ambari-server/target
+    #${rootDir}/zeppelin-zengine-new/zeppelin-zengine/target
+    #${rootDir}/zeppelin-new/zeppelin-server/target
+    #/home/user/dependent-tests-impact/experiments/activemq-new/activemq-camel/target
 )
 # Classpath to run the new subject's automatically-generated and human-written tests.
 nextExpCP=(
-#  ${rootDir}/impact-tools/*:${rootDir}/crystal/bin/:${rootDir}/crystal/libs/lib/*
-#  ${rootDir}/impact-tools/*:${rootDir}/jfreechart-1.0.16/bin/:${rootDir}/jfreechart-1.0.16/lib/*
-#  ${rootDir}/impact-tools/*:${rootDir}/jodatime-d6791cb5f9/bin/:${rootDir}/jodatime-d6791cb5f9/resources/:${rootDir}/jodatime-d6791cb5f9/lib/*
-#  ${rootDir}/impact-tools/*:${rootDir}/dynoptic-ea407ba0a750/bin/:${rootDir}/synoptic/lib/*:${rootDir}/synoptic-ea407ba0a750/bin/:${rootDir}/daikonizer-ea407ba0a750/bin/
-#  ${rootDir}/impact-tools/*:${rootDir}/xml-security-1_2_0/bin/:${rootDir}/xml-security-commons/bin/:${rootDir}/xml-security-1_2_0/data/:${rootDir}/xml-security-commons/libs/*
-#  ${rootDir}/impact-tools/*:${rootDir}/ambari-new/ambari-server/target/classes/:${rootDir}/ambari-new/ambari-server/target/test-classes/:${rootDir}/ambari-new/ambari-server/target/dependency/*:${rootDir}/ambari-new/ambari-server/target/randoop/bin/:/usr/lib/jvm/java-7-oracle/jre/lib/*:
-#  ${rootDir}/impact-tools/*:${rootDir}/zeppelin-zengine-new/zeppelin-zengine/target/classes/:${rootDir}/zeppelin-zengine-new/zeppelin-zengine/target/test-classes/:${rootDir}/zeppelin-zengine-new/zeppelin-zengine/target/dependency/*:${rootDir}/zeppelin-zengine-new/zeppelin-zengine/target/randoop/bin/:/usr/lib/jvm/java-7-oracle/jre/lib/*:
-  ${rootDir}/impact-tools/*:${rootDir}/zeppelin-new/zeppelin-server/target/classes/:${rootDir}/zeppelin-new/zeppelin-server/target/test-classes/:${rootDir}/zeppelin-new/zeppelin-server/target/dependency/*:${rootDir}/zeppelin-new/zeppelin-server/target/randoop/bin/:/usr/lib/jvm/java-7-oracle/jre/lib/*:
+   #${rootDir}/impact-tools/*:${rootDir}/crystal/bin/:${rootDir}/crystal/libs/lib/*
+   #${rootDir}/impact-tools/*:${rootDir}/jfreechart-1.0.16/bin/:${rootDir}/jfreechart-1.0.16/lib/*
+   #${rootDir}/impact-tools/*:${rootDir}/jodatime-d6791cb5f9/bin/:${rootDir}/jodatime-d6791cb5f9/resources/:${rootDir}/jodatime-d6791cb5f9/lib/*
+   #${rootDir}/impact-tools/*:${rootDir}/dynoptic-ea407ba0a750/bin/:${rootDir}/synoptic/lib/*:${rootDir}/synoptic-ea407ba0a750/bin/:${rootDir}/daikonizer-ea407ba0a750/bin/
+   #${rootDir}/impact-tools/*:${rootDir}/xml-security-1_2_0/bin/:${rootDir}/xml-security-commons/bin/:${rootDir}/xml-security-1_2_0/data/:${rootDir}/xml-security-commons/libs/*
+   ${rootDir}/impact-tools/*:${rootDir}/ambari-new/ambari-server/target/classes/:${rootDir}/ambari-new/ambari-server/target/test-classes/:${rootDir}/ambari-new/ambari-server/target/dependency/*:${rootDir}/ambari-new/ambari-server/target/randoop/bin/:/usr/lib/jvm/java-7-oracle/jre/lib/*:
+   #${rootDir}/impact-tools/*:${rootDir}/zeppelin-zengine-new/zeppelin-zengine/target/classes/:${rootDir}/zeppelin-zengine-new/zeppelin-zengine/target/test-classes/:${rootDir}/zeppelin-zengine-new/zeppelin-zengine/target/dependency/*:${rootDir}/zeppelin-zengine-new/zeppelin-zengine/target/randoop/bin/:/usr/lib/jvm/java-7-oracle/jre/lib/*:
+   #${rootDir}/impact-tools/*:${rootDir}/zeppelin-new/zeppelin-server/target/classes/:${rootDir}/zeppelin-new/zeppelin-server/target/test-classes/:${rootDir}/zeppelin-new/zeppelin-server/target/dependency/*:${rootDir}/zeppelin-new/zeppelin-server/target/randoop/bin/:/usr/lib/jvm/java-7-oracle/jre/lib/*:
+   #/home/user/dependent-tests-impact/experiments/impact-tools/*:/home/user/dependent-tests-impact/experiments/activemq-new/activemq-camel/target/dependency/*:/home/user/dependent-tests-impact/experiments/activemq-new/activemq-camel/target/classes:/home/user/dependent-tests-impact/experiments/activemq-new/activemq-camel/target/randoop/bin:/home/user/dependent-tests-impact/experiments/activemq-new/activemq-camel/target/test-classes:
 )
 # Directory containing the old subject's src directory. These directories contain projects that were originally built with Ant. 
 antExp=(
-  ${rootDir}/crystalvc
-  ${rootDir}/jfreechart-1.0.15
-  ${rootDir}/jodatime-b609d7d66d
-  ${rootDir}/dynoptic
-  ${rootDir}/xml-security-orig-v1
+  #${rootDir}/crystalvc
+  #${rootDir}/jfreechart-1.0.15
+  #${rootDir}/jodatime-b609d7d66d
+  #${rootDir}/dynoptic
+  #${rootDir}/xml-security-orig-v1
 )
 # Directory containing the new subject's src directory. These directories contain projects that were originally built with Ant. 
 antNextExp=(
-  ${rootDir}/crystal
-  ${rootDir}/jfreechart-1.0.16
-  ${rootDir}/jodatime-d6791cb5f9
-  ${rootDir}/dynoptic-ea407ba0a750
-  ${rootDir}/xml-security-1_2_0
+  #${rootDir}/crystal
+  #${rootDir}/jfreechart-1.0.16
+  #${rootDir}/jodatime-d6791cb5f9
+  #${rootDir}/dynoptic-ea407ba0a750
+  #${rootDir}/xml-security-1_2_0
 )
 # Directory containing the old subject's src directory. These directories contain projects that were originally built with Maven. 
 antMvnExp=(
   ${rootDir}/ambari/ambari-server
-  ${rootDir}/zeppelin/zeppelin-zengine
-  ${rootDir}/zeppelin/zeppelin-server
+  #${rootDir}/zeppelin/zeppelin-zengine
+  #${rootDir}/zeppelin/zeppelin-server
+  #/home/user/dependent-tests-impact/experiments/activemq-old/activemq-camel/src
 )
 # Directory containing the new subject's src directory. These directories contain projects that were originally built with Maven. 
 antMvnNextExp=(
   ${rootDir}/ambari-new/ambari-server
-  ${rootDir}/zeppelin-zengine-new/zeppelin-zengine
-  ${rootDir}/zeppelin-new/zeppelin-server
+  #${rootDir}/zeppelin-zengine-new/zeppelin-zengine
+  #${rootDir}/zeppelin-new/zeppelin-server
+  #/home/user/dependent-tests-impact/experiments/activemq-new/activemq-camel/src
 )
