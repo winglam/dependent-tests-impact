f92027b2627dff446a8622790241334aa3587dfb
Reed Oei <oei.reed@gmail.com>
2018-01-16 12:27:35 -0600

    Added functions for working with dependencies.

--- dt-impact-tracer/src/main/java/edu/washington/cs/dt/impact/tools/DependentTestFinder.java
+++ dt-impact-tracer/src/main/java/edu/washington/cs/dt/impact/tools/DependentTestFinder.java
@@ -15,6 +15,7 @@ import edu.washington.cs.dt.runners.FixedOrderRunner;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -143,5 +144,68 @@ public class DependentTestFinder {
         }
 
         return primeOrder;
+    }
+
+    /**
+     * Generates a test order containing all the tests in order, with the dependent test coming at
+     * the end, respecting all dependencies.
+     * @param order The order of tests to run before the dependent test. Not modified.
+     * @return The results from running the tests in that order.
+     */
+    private TestExecResult makeAndRunTestOrder(final List<String> order) {
+        List<String> newOrder = new ArrayList<>(order);
+        newOrder.add(dependentTestName);
+
+        newOrder = TestOrderGenerator.generateTestOrder(newOrder, knownDependencies);
+
+        return runTestOrder(newOrder);
+    }
+
+    private void addDependency(final String testName, final String dependency) {
+        knownDependencies.compute(testName, (t, dependencies) -> {
+            if (dependencies == null) {
+                dependencies = new HashSet<>();
+            }
+
+            dependencies.add(dependency);
+            return dependencies;
+        });
+    }
+
+    public Map<String, Set<String>> getKnownDependencies() {
+        return knownDependencies;
+    }
+
+    /**
+     * Takes a list of dtFinders and merges the dependency information from all of them into one map.
+     */
+    public static Map<String, Set<String>> mergeDependencies(final List<DependentTestFinder> dtFinders) {
+        return dtFinders.stream()
+                .map(DependentTestFinder::getKnownDependencies)
+                .reduce(new HashMap<>(), DependentTestFinder::mergeDependencies);
+    }
+
+    /**
+     * Takes two maps of dependencies and combines them. If both maps contain the same key, the sets
+     * of dependencies will be merged.
+     *
+     * Does not modify either of the original maps.
+     * @param a The first map of dependencies.
+     * @param b The second map of dependencies.
+     * @return A map contains all of the dependencies.
+     */
+    public static Map<String, Set<String>> mergeDependencies(final Map<String, Set<String>> a,
+                                                             final Map<String, Set<String>> b) {
+        final Map<String, Set<String>> result = new HashMap<>(a);
+
+        b.forEach((testName, dependencies) ->
+                // If any key is in both maps, then merge the sets using this function.
+                result.merge(testName, dependencies, (aDependencies, bDependencies) -> {
+                    aDependencies.addAll(bDependencies);
+
+                    return aDependencies;
+                }));
+
+        return result;
     }
 }
