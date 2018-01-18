2c6bfbeaa7ca40a5a0995b0dce18f0bb3eaea9d6
Reed Oei <oei.reed@gmail.com>
2018-01-17 18:01:10 -0600

    Added verification that dependencies were found correctly.

---
+++ dt-impact-tracer/src/main/java/edu/washington/cs/dt/impact/tools/DependencyVerificationException.java
@@ -0,0 +1,7 @@
+package edu.washington.cs.dt.impact.tools;
+
+public class DependencyVerificationException extends Exception {
+    public DependencyVerificationException(final String message) {
+        super(message);
+    }
+}
