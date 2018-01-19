c44bad0c69d5cab4c5e7f6484728f593c85bda79
Reed Oei <oei.reed@gmail.com>
2018-01-18 19:10:14 -0600

    Modified Parallel DT finder to return map to set of TestData objects.

--- dt-impact-tracer/src/main/java/edu/washington/cs/dt/impact/data/TestData.java
+++ dt-impact-tracer/src/main/java/edu/washington/cs/dt/impact/data/TestData.java
@@ -1,9 +1,7 @@
 package edu.washington.cs.dt.impact.data;
 
 import edu.washington.cs.dt.RESULT;
-import edu.washington.cs.dt.TestExecResult;
 
-import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
@@ -32,54 +30,14 @@ public class TestData {
 
     @Override
     public String toString() {
-        // If the intended behavior is the revealed behavior, then the before/after tests are the
-        // tests needs before/after to make it have the intended behavior.
-        if (intended == revealed) {
-            return  "Test: " + dependentTest + "\n" +
-                    "Intended behavior: " + intended + "\n" +
-                    "when executed after: " + beforeTests + "\n" +
-                    "and executed before: " + afterTests + "\n";
-
-        } else {
-            // Otherwise, the before/after tests are the tests that cause the revealed behavior to be
-            // different from the intended behavior.
-            return  "Test: " + dependentTest + "\n" +
-                    "Intended behavior: " + intended + "\n" +
-                    "The revealed behavior: " + revealed + "\n" +
-                    "when executed after: " + beforeTests + "\n" +
-                    "and executed before: " + afterTests + "\n" +
-                    "in the order: " + revealingOrder;
-        }
-    }
-
-    public TestData mergeWith(final TestData other) {
-        final Set<String> newBeforeTests = new HashSet<>(beforeTests);
-        newBeforeTests.addAll(other.beforeTests);
-
-        final Set<String> newAfterTests = new HashSet<>(afterTests);
-        newAfterTests.addAll(other.afterTests);
-
-        return new TestData(dependentTest, intended, newBeforeTests, newAfterTests, revealed);
-    }
-
-    public void addBefore(final String testName) {
-        beforeTests.add(testName);
-    }
-
-    public void addAfter(final String testName) {
-        afterTests.add(testName);
-    }
-
-    public boolean contains(final String testName) {
-        return containsBefore(testName) || containsAfter(testName);
-    }
-
-    public boolean containsBefore(String testName) {
-        return beforeTests.contains(testName);
-    }
-
-    public boolean containsAfter(String testName) {
-        return afterTests.contains(testName);
+        // Otherwise, the before/after tests are the tests that cause the revealed behavior to be
+        // different from the intended behavior.
+        return  "Test: " + dependentTest + "\n" +
+                "Intended behavior: " + intended + "\n" +
+                "when executed after: " + beforeTests + "\n" +
+                "and executed before: " + afterTests + "\n" +
+                "The revealed behavior: " + revealed + "\n" +
+                "in the order: " + revealingOrder;
     }
 
     /**
