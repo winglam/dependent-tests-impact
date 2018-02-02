491aa7b1fb0e90accfb9889d81808ae6964c29ca
Reed Oei <oei.reed@gmail.com>
2018-02-02 10:31:44 -0600

    Wrote fromString method for TestData.

--- dt-impact-tracer/src/main/java/edu/washington/cs/dt/impact/data/TestData.java
+++ dt-impact-tracer/src/main/java/edu/washington/cs/dt/impact/data/TestData.java
@@ -2,9 +2,11 @@ package edu.washington.cs.dt.impact.data;
 
 import edu.washington.cs.dt.RESULT;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 import java.util.Set;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class TestData {
@@ -30,10 +32,63 @@ public class TestData {
         this.revealingOrder = revealingOrder;
     }
 
+    /**
+     * Trims whitespace.
+     * @param str The string to parse. Should look like "[a, b, c, d]"
+     * @return The stream of strings.
+     */
+    private static Stream<String> parseStrToStream(final String str) {
+        return Arrays.stream(str.substring(1, str.length() - 1).split(","))
+                .map(String::trim);
+    }
+
+    /**
+     * Parses the string for the information contained in the test data object.
+     * Format is the same as the output format from TestData#toString().
+     */
+    public static TestData fromString(final String str) {
+        final String[] lines = str.split("\n");
+
+        // Validate we have the correct number of lines and all lines contain ':'. If they don't,
+        // then we will get ArgumentOutOfBoundsExceptions below.
+        if (lines.length != 6) {
+            throw new IllegalArgumentException("The string passed in does not contain the correct " +
+                    "number of lines! Expected: 6, Got: " + lines.length);
+        }
+
+        if (!Arrays.stream(lines).allMatch(s -> s.contains(":"))) {
+            throw new IllegalArgumentException("All lines must contain ':'.");
+        }
+
+        final String[] nameLine = lines[0].split(":");
+        final String dependentTest = nameLine[1].trim();
+
+        final String[] intendedLine = lines[1].split(":");
+        final RESULT intended = RESULT.valueOf(intendedLine[1].trim());
+
+        final String[] beforeTestsLine = lines[2].split(":");
+        final Set<String> beforeTests =
+                parseStrToStream(beforeTestsLine[1].trim())
+                .collect(Collectors.toSet());
+
+        final String[] afterTestsLine = lines[3].split(":");
+        final Set<String> afterTests =
+                parseStrToStream(afterTestsLine[1].trim())
+                .collect(Collectors.toSet());
+
+        final String[] revealedLine = lines[4].split(":");
+        final RESULT revealed = RESULT.valueOf(revealedLine[1].trim());
+
+        final String[] revealingOrderLine  = lines[5].split(":");
+        final List<String> revealingOrder =
+                parseStrToStream(revealingOrderLine[1].trim())
+                .collect(Collectors.toList());
+
+        return new TestData(dependentTest, intended, beforeTests, afterTests, revealed, revealingOrder);
+    }
+
     @Override
     public String toString() {
-        // Otherwise, the before/after tests are the tests that cause the revealed behavior to be
-        // different from the intended behavior.
         return  "Test: " + dependentTest + "\n" +
                 "Intended behavior: " + intended + "\n" +
                 "when executed after: " + beforeTests + "\n" +
@@ -87,6 +142,27 @@ public class TestData {
                 beforeTests.stream().map(testOrder::indexOf),
                 afterTests.stream().map(testOrder::indexOf)
         );
+    }
+
+    @Override
+    public final boolean equals(final Object o) {
+        if (o instanceof TestData) {
+            final TestData other = (TestData) o;
+
+            return  dependentTest.equals(other.dependentTest) &&
+                    intended.equals(other.intended) &&
+                    beforeTests.equals(other.beforeTests) &&
+                    afterTests.equals(other.afterTests) &&
+                    revealed.equals(other.revealed) &&
+                    revealingOrder.equals(other.revealingOrder);
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(dependentTest, intended, beforeTests, afterTests, revealed, revealingOrder);
     }
 }
 
